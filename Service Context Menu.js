c810df8185ec7f890df6726f3668b73cc9a2da68
ssorpg <42558066+ssorpg@users.noreply.github.com>
2023-10-28 16:31:25 -0400

    Full conversion to context menu complete

--- Service Context Menu.js
+++ Service Context Menu.js
@@ -17,11 +17,29 @@
 
 	const { Item, DOMEditor, WebcallScheduler } = window.ssorpg1;
 
+	let contextMenu = null;
+	let mouseIsOverContextMenu = false;
+
 	DOMEditor.getInventoryCells().forEach((cell) => cell.addEventListener("contextmenu", openContextMenu));
+	document.addEventListener("mousedown", () => {
+		if (!contextMenu) {
+			return;
+		}
+
+		if (mouseIsOverContextMenu) {
+			return;
+		}
+
+		contextMenu.remove();
+	});
 
 	function openContextMenu(e) {
 		if (window.marketScreen != "buy") {
 			return;
+		}
+
+		if (contextMenu) {
+			contextMenu.remove();
 		}
 
 		e.preventDefault();
@@ -33,56 +51,62 @@
 		}
 
 		const item = new Item(itemElement);
-		let catname;
-		let textContent;
+		let catname = null;
+		let cat = null;
+		let level = null;
 
 		if (item.category == "armour") {
 			catname = "Engineer";
-			textContent = "Services - Repair";
+			cat = "Services - Repair";
+			level = item.itemData.shop_level;
 		}
 		else if (item.itemData.needdoctor == "1") {
 			catname = "Doctor";
-			textContent = "Services - Medical";
+			cat = "Services - Medical";
+			level = item.itemData.level;
 		}
 		else if (item.itemData.needcook == "1") {
 			catname = "Chef";
-			textContent = "Services - Cooking";
+			cat = "Services - Cooking";
+			level = item.itemData.level;
 		}
 		else {
 			return;
 		}
 
-		const contextMenu = document.createElement("div");
+		contextMenu = document.createElement("div");
+		contextMenu.id = "ssorpg1_ContextMenu";
 		contextMenu.style.visibility = "hidden";
-		contextMenu.style.id = "ssorpg1_ContextMenu";
 		contextMenu.style.backgroundColor = "black";
 		contextMenu.style.border = "1px solid white";
 		contextMenu.style.zIndex = "20";
 		contextMenu.style.textAlign = "left";
 		contextMenu.style.position = "absolute";
 		contextMenu.style.width = "140px";
-		DOMEditor.contextMenuCorrection(contextMenu);
+		mouseIsOverContextMenu = false;
+		contextMenu.addEventListener("mouseenter", () => mouseIsOverContextMenu = true);
+		contextMenu.addEventListener("mouseleave", () => mouseIsOverContextMenu = false);
 
 		const title = document.createElement("div");
 		title.style.textAlign = "center";
 		title.textContent = item.name;
 
 		const button = document.createElement("button");
-		button.textContent = "Find";
+		button.textContent = "Find " + catname;
 		button.style.width = "100%";
-		button.dataset.find_level = item.itemData.find_level;
+		button.dataset.level = (Number(level) - 5).toString();
 		button.dataset.catname = catname;
-		button.dataset.textContent = textContent;
-		button.addEventListener("mousedown", WebcallScheduler.enqueue(async (e) => await findService(e)));
+		button.dataset.cat = cat;
+		button.addEventListener("mousedown", () => findService(button));
 
 		contextMenu.appendChild(title);
 		contextMenu.appendChild(button);
+		window.inventoryHolder.appendChild(contextMenu);
+		DOMEditor.contextMenuCorrection(contextMenu);
 		contextMenu.style.visibility = "visible";
 	}
 
-	async function findService(e) {
-		const button = e.currentTarget;
-
+	function findService(button) {
 		const {
 			searchField,
 			categoryChoice,
@@ -90,13 +114,16 @@
 			makeSearch
 		} = DOMEditor.getTradeSearchElements();
 
-		searchField.value = button.dataset.find_level;
+		searchField.value = button.dataset.level;
 		categoryChoice.dataset.catname = button.dataset.catname;
 		categoryChoice.dataset.cattype = "service";
-		cat.textContent = button.dataset.textContent;
+		cat.textContent = button.dataset.cat;
 		makeSearch.disabled = false;
-		document.getElementById("ssorpg1_ContextMenu").remove();
-		window.search();
-		return true;
+
+		contextMenu.remove();
+		WebcallScheduler.enqueue(async () => {
+			window.search();
+			return true;
+		});
 	}
 })();
